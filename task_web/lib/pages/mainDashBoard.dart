9a1f51e77e5c51766210cee40140be1565776463
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2023-08-24 23:57:10 +0530

    Dashboard Update

--- task_web/lib/pages/mainDashBoard.dart
+++ task_web/lib/pages/mainDashBoard.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../components.dart';
+import '../methods/sideDropMenu.dart';
+import 'loginPage.dart';
 
 class MainDashBoard extends StatefulWidget {
   const MainDashBoard({super.key});
@@ -8,10 +13,36 @@ class MainDashBoard extends StatefulWidget {
 }
 
 class _MainDashBoardState extends State<MainDashBoard> {
+
+  String userName = "";
+  String firstName = "";
+  String lastName = "";
+  String phone = "";
+  String userRole = "";
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  void loadData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      userName = prefs.getString('user_name') ?? "";
+      firstName = prefs.getString('first_name') ?? "";
+      lastName = prefs.getString('last_name') ?? "";
+      phone = prefs.getString('phone') ?? "";
+      userRole = prefs.getString('user_role') ?? "";
+
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         backgroundColor: Colors.white,
         title: Text(
           'CBS Task System',
@@ -21,12 +52,31 @@ class _MainDashBoardState extends State<MainDashBoard> {
           ),
         ),
         actions: [
+
+          Column(
+            children: [
+              Text('',style: TextStyle(
+                color: Colors.black,
+              ),),
+              Text(
+                "$firstName $lastName",
+                style: const TextStyle(color: Colors.black, fontSize: 20.0),
+              ),
+            ],
+          ),
+
           IconButton(
-              onPressed: () {},
+              onPressed: ()
+              {
+                showPopupMenu(context);
+              },
+
+
               icon: Icon(
                 Icons.arrow_drop_down,
                 color: Colors.black,
               )),
+
         ],
       ),
     );
