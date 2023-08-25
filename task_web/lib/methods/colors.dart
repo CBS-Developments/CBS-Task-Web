ed1acd10fdf3430018ba73ace8d6105de482a76a
Dinethri Gunawardhana <dinethriupeksha200@gmail.com>
2023-08-25 12:54:59 +0530

    color dart add

---
+++ task_web/lib/methods/colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColor {
+  static Color filteT = const Color(0xFF8B8BA7);
+  static Color filterDrop = const Color(0xFF512B81);
+  static Color drawerLight = const Color(0xFF8CABFF);
+  static Color drawerDark = const Color(0xFF512B81);
+
+}
