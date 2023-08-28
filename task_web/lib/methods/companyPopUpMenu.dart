1c8b80b348ecf28fadd537f0916854ac3251b80c
Dinethri Gunawardhana <dinethriupeksha200@gmail.com>
2023-08-29 00:15:20 +0530

    Update companyPopUpMenu.dart

--- task_web/lib/methods/companyPopUpMenu.dart
+++ task_web/lib/methods/companyPopUpMenu.dart
@@ -29,7 +29,110 @@ void companyPopupMenu(BuildContext context) {
     topOffset + button.size.height,
   );
 
-  final companyNames = ['Company 01', 'Company 02', 'Company 03', 'Company 04'];
+  final companyNames = [
+    '-All-',
+    'A W M Riza',
+    'Academy of Digital Business Pvt. Ltd',
+    'Ajay Hathiramani',
+    'Andea Pereira',
+    'Andrew Downal',
+    'Asanga Karunarathne',
+    'Ashish Debey',
+    'Askalu Lanka Pvt. Ltd',
+    'Axis Tech Lanka (Pvt) Ltd',
+    'B C M Azwath',
+    'Ceylon Secretarial Services Pvt. Ltd',
+    'Codify Lanka Pvt. Ltd',
+    'Colonel Sujith Jayasekera',
+    'Compume (Pvt) Ltd',
+    'Corporate Business Solutions Pvt. Ltd',
+    'Courtesy Law Lanka Pvt. Ltd',
+    'Damith Gangodawilage',
+    'David Murray',
+    'DBA Alumni',
+    'Deepani Attanayake',
+    'Denver De Zylva',
+    'Deshan Senadheera',
+    'Dilhan Fernando',
+    'Dinoo Perera',
+    'Directpay (Pvt) Ltd',
+    'DN Thurairajah & Co.',
+    'Dr. Ishantha Jayasekera',
+    'Dr. Shahani Markus',
+    'E A Bimal Silva',
+    'Eksath Perera',
+    'Emojot Inc.',
+    'Emojot Pvt. Ltd',
+    'Fawas Ashraff',
+    'Fernando Ventures Pvt. Ltd',
+    'GK Wijayananada',
+    'Gullies Beauty Care',
+    'Hemal Kannangara',
+    'Himali De Silva',
+    'Idak Ceylon (Pvt) Ltd',
+    'Imate Construction',
+    'Ishan Dantanarayana',
+    'Jagath Pathirane',
+    'Jithain Hathiramani',
+    'JK Chambers/Kanchana Senanayake',
+    'Kalpitiya Discovery Diving Pvt. Ltd',
+    'Kelsey Services/Kavan Weerasinghe',
+    'L.D Wijerathne',
+    'Lloyd Mills Pvt Ltd',
+    'Lowcodeminds (Pvt) Ltd',
+    'M R Muthalif',
+    'Madu Rathnayake',
+    'Maithri Liyange',
+    'Mars Global Services Pvt. Ltd',
+    'Maryse Perers',
+    'Media Box/Ayesha',
+    'Migara Perera',
+    'Milinda Wattegerda',
+    'Mithun Liyanage',
+    'Mr. Lakshman Jayathilake',
+    'Nature Confort Lanka Holdings Pvt. Ltd',
+    'Nausha Raheem',
+    'Naveen Wijetunga',
+    'Nilangani De Silva',
+    'Nirmana Traders/Surath Herath',
+    'Nitmark Technologies Pvt. Ltd',
+    'Nugawela Transport',
+    'Off2 Lanka',
+    'Paymedia Pvt. Ltd',
+    'Pelicancube (Pvt) Ltd',
+    'Pradipa Jayathilaka',
+    'Prasanna Wijesiri',
+    'Rajeeve Goonetileke',
+    'Rasanga Shanaka',
+    'Ravin',
+    'Reena',
+    'Ruchika Roonahewa',
+    'Rumesh Athukorala',
+    'Sachnitha Rajith Ponnamperuma',
+    'Saliya Silva',
+    'Samantha Maithriwardena',
+    'Sameera Subashingha',
+    'Sampath Gunawardena',
+    'Sanjeeva Abyewardena',
+    'Sayura Beer Shop/Sunil Punchibandara',
+    'Shanil Fernando',
+    'Shirani Kulasinghe',
+    'Sonali Wicremaratne',
+    'Squarehub (Pvt) Ltd',
+    'Stephen Paulraj',
+    'Sumudu Kumara Gunawarden',
+    'Suren Karunakaran',
+    'Tanya Gunasekera',
+    'Taxperts Lanka Pvt. Ltd',
+    'Tesman Melani',
+    'Tharaka',
+    'Tharumal Wijesimghe',
+    'The Embazzy',
+    'The Headmasters Pvt. Ltd',
+    'Thingerbits Pvt. Ltd',
+    'Tikiri Banda & Sons/Dr. Bandara',
+    'Univiser (Pvt) Ltd',
+    'UP Weerasinghe Properties Pvt. Ltd'];
 
   final companyDropdownState =
   Provider.of<CompanyDropdownState>(context, listen: false); // Use the existing instance
@@ -58,15 +161,6 @@ void companyPopupMenu(BuildContext context) {
       print('Selected item: $selectedValue');
 
       // Inside your taskPopupMenu method
-      if (value == 0) {
-        companyDropdownState.value = 'Company 01';
-      } else if (value == 1) {
-        companyDropdownState.value = 'Company 02';
-      } else if (value == 2) {
-        companyDropdownState.value = 'Company 03';
-      } else if (value == 3) {
-        companyDropdownState.value = 'Company 04';
-      }
     }
   });
 }
