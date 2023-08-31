4af953eba7b5c6722a021c65aa58130697f2df71
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2023-08-31 11:04:33 +0530

    Update openTaskBox.dart

--- task_web/lib/pages/openTaskBox.dart
+++ task_web/lib/pages/openTaskBox.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task_web/methods/colors.dart';
 import '../methods/taskTable.dart';
 
 class TaskDetailsDialog extends StatelessWidget {
@@ -13,41 +14,152 @@ class TaskDetailsDialog extends StatelessWidget {
           width: 800, // Set the width of the dialog
           height: 400, // Set the height of the dialog
           child: SingleChildScrollView(
-            child: Row(
-              children: [
-                Container(
-                  width: 500,
-                  height: 400,
-                  color: Colors.greenAccent,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('${task.taskTitle}',style: TextStyle(
-                        fontSize: 16,
-                      ),),
-                      Text('${task.taskId}',style: TextStyle(
-                        fontSize: 12,
-                        color: Colors.grey,
+              child: Row(
+            children: [
+              Container(
+                width: 500,
+                height: 400,
+                color: Colors.greenAccent,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          '${task.taskTitle}',
+                          style: TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                        IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.remove_red_eye_outlined,
+                              color: Colors.black,
+                            ))
+                      ],
+                    ),
+                    Text(
+                      '${task.taskId}',
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.black87,
                       ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      width: 450,
+                      height: 300,
+                      color: Colors.grey.shade700,
+                      child: Row(
+                        children: [
+                          SizedBox(
+                            width: 150,
+                            height: 300,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+
+                                Row(
+                                  children: [
+                                    Icon(Icons.calendar_month_rounded,size: 15,),
+                                    Padding(
+                                      padding: const EdgeInsets.all(4.0),
+                                      child: Text(
+                                        'Start',
+                                        style: TextStyle(
+                                            fontSize: 14,
+                                            color: AppColor.drawerLight),
+                                      ),
+                                    ),
+
+                                    Icon(Icons.arrow_forward,color: AppColor.drawerLight,size: 15,),
+
+                                    Padding(
+                                      padding: const EdgeInsets.all(4.0),
+                                      child: Text(
+                                        'Due',
+                                        style: TextStyle(
+                                            fontSize: 14,
+                                            color: AppColor.drawerLight),
+                                      ),
+                                    ),
+
+                                  ],
+                                ),
+
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 18,bottom: 4),
+                                  child: Text(
+                                    'Company',
+                                    style: TextStyle(
+                                        fontSize: 14,
+                                        color: AppColor.drawerLight),
+                                  ),
+                                ),
+
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 18,bottom: 4),
+                                  child: Text(
+                                    'Assign To',
+                                    style: TextStyle(
+                                        fontSize: 14,
+                                        color: AppColor.drawerLight),
+                                  ),
+                                ),
+
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 18,bottom: 4),
+                                  child: Text(
+                                    'Priority',
+                                    style: TextStyle(
+                                        fontSize: 14,
+                                        color: AppColor.drawerLight),
+                                  ),
+                                ),
+
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 18,bottom: 4),
+                                  child: Text(
+                                    'Status',
+                                    style: TextStyle(
+                                        fontSize: 14,
+                                        color: AppColor.drawerLight),
+                                  ),
+                                ),
+
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 18,bottom: 4),
+                                  child: Text(
+                                    'Created By',
+                                    style: TextStyle(
+                                        fontSize: 14,
+                                        color: AppColor.drawerLight),
+                                  ),
+                                ),
+
+                              ],
+                            ),
+                          ),
+                          VerticalDivider(
+                            thickness: 2,
+                          )
+                        ],
                       ),
-
-                    ],
-                  ),
-
-
-
+                    )
+                  ],
                 ),
-
-                Container(
-                  width: 300,
-                  height: 400,
-                  color: Colors.lightBlueAccent,
-
-
-                )
-              ],
-            )
-          )),
+              ),
+              Container(
+                width: 300,
+                height: 400,
+                color: Colors.lightBlueAccent,
+              )
+            ],
+          ))),
       actions: <Widget>[
         ElevatedButton(
           onPressed: () {
