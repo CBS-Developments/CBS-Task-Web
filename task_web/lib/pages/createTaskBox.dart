4a93ec8c4480f1ca277f77e5e6213e930adce40a
Dinethri Gunawardhana <dinethriupeksha200@gmail.com>
2023-08-31 20:25:24 +0530

    Update createTaskBox.dart

--- task_web/lib/pages/createTaskBox.dart
+++ task_web/lib/pages/createTaskBox.dart
@@ -1,6 +1,27 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../components.dart';
 import '../methods/colors.dart';
+
+enum menuitem {
+  item1,
+  item2,
+  item3,
+  item4,
+  item5,
+  item6,
+  item7,
+  item8,
+  item9,
+  item10,
+  item11
+}
 
 class CreateTaskDialog extends StatefulWidget {
   const CreateTaskDialog({Key? key}) : super(key: key);
@@ -10,25 +31,240 @@ class CreateTaskDialog extends StatefulWidget {
 }
 
 class _CreateTaskDialogState extends State<CreateTaskDialog> {
-  TextEditingController _titleController = TextEditingController();
-  TextEditingController _descriptionController = TextEditingController();
+  bool titleValidation = false;
+  bool subTaskTitleValidation = false;
+  bool descriptionValidation = false;
+  int taskType = 1;
+  String taskTypeString = "Top Urgent";
+  // ignore: unused_field
+  menuitem _mitem = menuitem.item1;
+  late String userName;
+  late String firstName;
+  late String lastName;
+  List<String> assignTo = [];
+
+  TextEditingController titleController = TextEditingController();
+  TextEditingController subTitleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
   TextEditingController createTaskDueDateController = TextEditingController();
+  TextEditingController documentNumberController = TextEditingController();
+  TextEditingController assignToController = TextEditingController();
   bool _isEditingTitle = true;
   DateTime? _selectedDueDate; // Store the selected due date
 
   @override
-  void dispose() {
-    _titleController.dispose();
-    _descriptionController.dispose();
-    super.dispose();
+  void initState() {
+    super.initState();
+    retrieverData();
   }
+
+  void retrieverData() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      userName = (prefs.getString('user_name') ?? '');
+      firstName = (prefs.getString('first_name') ?? '').toUpperCase();
+      lastName = (prefs.getString('last_name') ?? '').toUpperCase();
+    });
+  }
+
+  Future<bool> mainTask(BuildContext context) async {
+    if (titleController.text.trim().isEmpty) {
+      setState(() {
+        titleValidation = true;
+        snackBar(context, "Task title can't be empty", Colors.redAccent);
+      });
+      return false;
+    } else {
+      setState(() {
+        titleValidation = false;
+      });
+    }
+
+    int timestamp = DateTime.now().millisecondsSinceEpoch;
+    int taskTimeStamp = timestamp;
+    var timeSt = taskTimeStamp;
+    var dt = DateTime.fromMillisecondsSinceEpoch(taskTimeStamp);
+    var stringDate = DateFormat('MM/dd/yyyy').format(dt);
+    var stringMonth = DateFormat('MM/yyyy').format(dt);
+    var taskId = "$userName#MT$taskTimeStamp";
+    var url = "http://dev.connect.cbs.lk/mainTask.php";
+    var data = {
+      "task_id": taskId,
+      "task_title": titleController.text,
+      "task_type": "$taskType",
+      "task_type_name": taskTypeString,
+      "due_date": createTaskDueDateController.text,
+      "task_create_by_id": userName,
+      "task_create_by": "$firstName $lastName",
+      "task_create_date": stringDate,
+      "task_create_month": stringMonth,
+      "task_created_timestamp": '$timeSt',
+      "task_status": "0",
+      "task_status_name": "Pending",
+      "task_reopen_by": "",
+      "task_reopen_by_id": "",
+      "task_reopen_date": "",
+      "task_reopen_timestamp": "0",
+      "task_finished_by": "",
+      "task_finished_by_id": "",
+      "task_finished_by_date": "",
+      "task_finished_by_timestamp": "0",
+      "task_edit_by": "",
+      "task_edit_by_id": "",
+      "task_edit_by_date": "",
+      "task_edit_by_timestamp": "0",
+      "task_delete_by": "",
+      "task_delete_by_id": "",
+      "task_delete_by_date": "",
+      "task_delete_by_timestamp": "0",
+      "source_from": dropdownvalue1,
+      "assign_to": assignToController.text,
+      "company": dropdownvalue3,
+      "document_number": documentNumberController.text,
+      "action_taken_by_id": "",
+      "action_taken_by": "",
+      "action_taken_date": "",
+      "action_taken_timestamp": "0"
+    };
+
+    http.Response res = await http.post(
+      Uri.parse(url),
+      body: data,
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      encoding: Encoding.getByName("utf-8"),
+    );
+
+    if (res.statusCode.toString() == "200") {
+      if (jsonDecode(res.body) == "true") {
+        if (!mounted) return true;
+        createTask(context, taskId);
+        return true;
+      } else {
+        if (!mounted) return false;
+        snackBar(context, "Error", Colors.red);
+      }
+    } else {
+      if (!mounted) return false;
+      snackBar(context, "Error", Colors.redAccent);
+    }
+    return true;
+  }
+  Future<bool> createTask(BuildContext context, var mainTaskId) async {
+    if (titleController.text.trim().isEmpty) {
+      setState(() {
+        titleValidation = true;
+        snackBar(context, "Task title can't be empty", Colors.redAccent);
+      });
+      return false;
+    } else {
+      setState(() {
+        titleValidation = false;
+      });
+    }
+
+    if (subTitleController.text.trim().isEmpty) {
+      setState(() {
+        subTaskTitleValidation = true;
+        snackBar(context, "Sub task title can't be empty", Colors.redAccent);
+      });
+      return false;
+    } else {
+      setState(() {
+        subTaskTitleValidation = false;
+      });
+    }
+
+    int timestamp = DateTime.now().millisecondsSinceEpoch;
+    int taskTimeStamp = timestamp;
+    var timeSt = taskTimeStamp;
+    var dt = DateTime.fromMillisecondsSinceEpoch(taskTimeStamp);
+    var stringDate = DateFormat('MM/dd/yyyy').format(dt);
+    var stringMonth = DateFormat('MM/yyyy').format(dt);
+    var taskId = "$userName#ST$taskTimeStamp";
+    var url = "http://dev.connect.cbs.lk/createTask.php";
+
+    var data = {
+      "task_id": taskId,
+      "main_task_id": "$mainTaskId",
+      "task_title": subTitleController.text,
+      "task_type": "$taskType",
+      "task_type_name": taskTypeString,
+      "due_date": createTaskDueDateController.text,
+      "task_description": descriptionController.text,
+      "task_create_by_id": userName,
+      "task_create_by": "$firstName $lastName",
+      "task_create_date": stringDate,
+      "task_create_month": stringMonth,
+      "task_created_timestamp": '$timeSt',
+      "task_status": "0",
+      "task_status_name": "Pending",
+      "action_taken_by_id": "",
+      "action_taken_by": "",
+      "action_taken_date": "",
+      "action_taken_timestamp": "0",
+      "task_reopen_by": "",
+      "task_reopen_by_id": "",
+      "task_reopen_date": "",
+      "task_reopen_timestamp": "0",
+      "task_finished_by": "",
+      "task_finished_by_id": "",
+      "task_finished_by_date": "",
+      "task_finished_by_timestamp": "0",
+      "task_edit_by": "",
+      "task_edit_by_id": "",
+      "task_edit_by_date": "",
+      "task_edit_by_timestamp": "0",
+      "task_delete_by": "",
+      "task_delete_by_id": "",
+      "task_delete_by_date": "",
+      "task_delete_by_timestamp": "0",
+      "source_from": dropdownvalue1,
+      "assign_to": assignToController.text,
+      "company": dropdownvalue3
+    };
+
+    http.Response res = await http.post(
+      Uri.parse(url),
+      body: data,
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      encoding: Encoding.getByName("utf-8"),
+    );
+
+    if (res.statusCode.toString() == "200") {
+      if (jsonDecode(res.body) == "true") {
+        if (!mounted) return true;
+        snackBar(context, "Done", Colors.green);
+        return true;
+      } else {
+        if (!mounted) return false;
+        snackBar(context, "Error", Colors.red);
+      }
+    } else {
+      if (!mounted) return false;
+      snackBar(context, "Error", Colors.redAccent);
+    }
+    return true;
+  }
+
+  // @override
+  // void dispose() {
+  //   _titleController.dispose();
+  //   _descriptionController.dispose();
+  //   super.dispose();
+  // }
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       title: _isEditingTitle
           ? TextField(
-        controller: _titleController,
+        controller: titleController,
         decoration: const InputDecoration(
           hintText: "Task Title",
         ),
@@ -55,7 +291,10 @@ class _CreateTaskDialogState extends State<CreateTaskDialog> {
                 ),
                 padding: const EdgeInsets.symmetric(horizontal: 10),
                 child: TextField(
-                  controller: _descriptionController,
+                  controller: descriptionController,
+                  textInputAction: TextInputAction.done,
+                  keyboardType: TextInputType.multiline,
+                  maxLines: null,
                   decoration: const InputDecoration(
                     hintText: "Add Description",
                     border: InputBorder.none,
@@ -166,11 +405,7 @@ class _CreateTaskDialogState extends State<CreateTaskDialog> {
                                     size: 16,
                                   ),
                                   Padding(
-                                    padding: const EdgeInsets.only(
-                                        left: 4,
-                                        bottom: 8,
-                                        top: 8,
-                                        right: 4),
+                                    padding: const EdgeInsets.only(left: 4, bottom: 8, top: 8, right: 4),
                                     child: Text(
                                       _selectedDueDate != null
                                           ? _selectedDueDate!
@@ -209,8 +444,8 @@ class _CreateTaskDialogState extends State<CreateTaskDialog> {
           TextButton(
             onPressed: () {
               // Perform the task addition logic here
-              String taskTitle = _titleController.text;
-              String taskDescription = _descriptionController.text;
+              String taskTitle = titleController.text;
+              String taskDescription = descriptionController.text;
               // Use the taskTitle and taskDescription for your logic
               // Close the dialog
               Navigator.of(context).pop();
@@ -221,3 +456,4 @@ class _CreateTaskDialogState extends State<CreateTaskDialog> {
     );
   }
 }
+
