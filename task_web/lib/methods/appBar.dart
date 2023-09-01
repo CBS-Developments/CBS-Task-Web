cf651c3af8c4ecb6a8403a0e4331528b3768c781
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2023-09-01 23:53:57 +0530

    AppBar added to methods

---
+++ task_web/lib/methods/appBar.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:task_web/methods/sideDropMenu.dart';
+
+class MyAppBar extends StatefulWidget {
+  const MyAppBar({super.key});
+
+  @override
+  State<MyAppBar> createState() => _MyAppBarState();
+}
+
+class _MyAppBarState extends State<MyAppBar> {
+
+
+  String userName = "";
+  String firstName = "";
+  String lastName = "";
+  String phone = "";
+  String userRole = "";
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  void loadData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      userName = prefs.getString('user_name') ?? "";
+      firstName = prefs.getString('first_name') ?? "";
+      lastName = prefs.getString('last_name') ?? "";
+      phone = prefs.getString('phone') ?? "";
+      userRole = prefs.getString('user_role') ?? "";
+
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      elevation: 0,
+      automaticallyImplyLeading: false,
+      backgroundColor: Colors.white,
+      title: const Text(
+        'CBS Task System',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 20,
+        ),
+      ),
+      actions: [
+
+        Column(
+          children: [
+            const Text('',style: TextStyle(
+              color: Colors.black,
+            ),),
+            Text(
+              "$firstName $lastName",
+              style: const TextStyle(color: Colors.black, fontSize: 18.0),
+            ),
+          ],
+        ),
+
+        const SizedBox(width: 10,),
+
+        IconButton(
+            onPressed: ()
+            {},
+            icon: const Icon(
+              Icons.notifications_none_rounded,
+              color: Colors.black,
+            )),
+
+
+        IconButton(
+            onPressed: ()
+            {
+              showPopupMenu(context);
+            },
+            icon: const Icon(
+              Icons.arrow_drop_down,
+              color: Colors.black,
+            )),
+
+      ],
+    );
+  }
+}
--- task_web/lib/pages/taskPage.dart
+++ task_web/lib/pages/taskPage.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:task_web/methods/appBar.dart';
 import 'package:task_web/methods/drawer.dart';
 import 'package:task_web/methods/taskTable.dart';
 import 'package:task_web/methods/upMainRow.dart';
 import 'package:task_web/pages/createTaskBox.dart';
 
 
-import '../methods/sideDropMenu.dart';
 
 class TaskPage extends StatefulWidget {
   const TaskPage({super.key});
@@ -45,55 +45,11 @@ class _TaskPageState extends State<TaskPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey.shade300,
-      appBar: AppBar(
-        elevation: 0,
-        automaticallyImplyLeading: false,
-        backgroundColor: Colors.white,
-        title: const Text(
-          'CBS Task System',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 20,
-          ),
-        ),
-        actions: [
 
-          Column(
-            children: [
-              const Text('',style: TextStyle(
-                color: Colors.black,
-              ),),
-              Text(
-                "$firstName $lastName",
-                style: const TextStyle(color: Colors.black, fontSize: 18.0),
-              ),
-            ],
-          ),
-
-          const SizedBox(width: 10,),
-
-          IconButton(
-              onPressed: ()
-              {},
-              icon: const Icon(
-                Icons.notifications_none_rounded,
-                color: Colors.black,
-              )),
-
-
-          IconButton(
-              onPressed: ()
-              {
-                showPopupMenu(context);
-              },
-              icon: const Icon(
-                Icons.arrow_drop_down,
-                color: Colors.black,
-              )),
-
-        ],
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(kToolbarHeight),
+        child: MyAppBar(),
       ),
-
       body:
       Row(
         children: [
