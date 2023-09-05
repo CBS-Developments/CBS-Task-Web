4b87177e445c8b5011e1702b8558644d385f4e45
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2023-09-05 10:43:00 +0530

    Create chatPage.dart

---
+++ task_web/lib/pages/chatPage.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import '../methods/appBar.dart';
+
+class ChatPage extends StatefulWidget {
+  const ChatPage({super.key});
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(kToolbarHeight),
+        child: MyAppBar(),
+      ),
+
+      
+    );
+  }
+}
