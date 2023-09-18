90b59e16c956b94663c1c13038afec8fdd941b81
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2023-09-18 23:12:27 +0530

    Update openTaskBox.dart

--- task_web/lib/pages/openTaskBox.dart
+++ task_web/lib/pages/openTaskBox.dart
@@ -1,13 +1,189 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:task_web/methods/colors.dart';
 import 'package:task_web/methods/subTaskTable.dart';
 import 'package:task_web/pages/editMainTask.dart';
+import '../components.dart';
 import '../methods/taskTable.dart';
+import 'package:http/http.dart' as http;
 
-class TaskDetailsDialog extends StatelessWidget {
+class TaskDetailsDialog extends StatefulWidget {
   final MainTask task;
 
   TaskDetailsDialog(this.task);
+
+  @override
+  State<TaskDetailsDialog> createState() => _TaskDetailsDialogState();
+}
+
+class _TaskDetailsDialogState extends State<TaskDetailsDialog> {
+
+  String userName;
+  String firstName;
+  String lastName;
+  String userRole;
+  String mainTaskId;
+  String mainTaskTitle;
+
+  TextEditingController mainTaskCommentController = TextEditingController();
+
+  Future<bool> createCommentMainTask(BuildContext context) async {
+    int timestamp = DateTime.now().millisecondsSinceEpoch;
+    int taskTimeStamp = timestamp;
+    var timeSt = taskTimeStamp;
+
+    var dt = DateTime.fromMillisecondsSinceEpoch(taskTimeStamp);
+
+    var stringDate = DateFormat('MM/dd/yyyy, hh:mm a').format(dt);
+
+    var string = mainTaskCommentController.text.replaceAll("u0027", "'");
+
+    var url = "http://dev.connect.cbs.lk/" + "createComment.php";
+    var data = {
+      "comment_id": "$timeSt",
+      "task_id": mainTaskId,
+      "comment": string,
+      "comment_create_by_id": userName,
+      "comment_create_by": "$firstName $lastName",
+      "comment_create_date": stringDate,
+      "comment_created_timestamp": "$timeSt",
+      "comment_status": "1",
+      "comment_edit_by": "",
+      "comment_edit_by_id": '',
+      "comment_edit_by_date": "",
+      "comment_edit_by_timestamp": "",
+      "comment_delete_by": "",
+      "comment_delete_by_id": "",
+      "comment_delete_by_date": "",
+      "comment_delete_by_timestamp": "",
+    };
+
+    http.Response res = await http.post(
+      url,
+      body: data,
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      encoding: Encoding.getByName("utf-8"),
+    );
+
+    if (res.statusCode.toString() == "200") {
+      if (jsonDecode(res.body) == "true") {
+        mainTaskCommentController.text = "";
+
+        setState(() {
+          getMainTaskCommentList(mainTaskId);
+        });
+        return true;
+      } else {
+        if (!mounted) return false;
+        snackBar(context, "Error", Colors.red);
+      }
+    } else {
+      if (!mounted) return false;
+      snackBar(context, "Error", Colors.redAccent);
+    }
+    return true;
+  }
+
+  Future<List<comment>?> getMainTaskCommentList(var taskId) async {
+    var data = {
+      "task_id": "$taskId",
+    };
+
+    const url = "http://dev.connect.cbs.lk/commentListById.php";
+    http.Response res = await http.post(
+      url,
+      body: data,
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      encoding: Encoding.getByName("utf-8"),
+    );
+
+    if (res.statusCode == 200) {
+      if (jsonDecode(res.body) != "Error") {
+        List jsonResponse = json.decode(res.body);
+        if (jsonResponse != null) {
+          return jsonResponse.map((sec) => comment.fromJson(sec)).toList();
+        }
+      } else {
+        return null;
+      }
+    } else {
+      throw Exception('Failed to load jobs from API');
+    }
+    return null;
+  }
+
+  Future<bool> deleteCommentInMainTask(
+      var commentId, var commentStatus, var userName, var name) async {
+    int timestamp = DateTime.now().millisecondsSinceEpoch;
+    int taskTimeStamp = timestamp;
+    var timeSt = taskTimeStamp;
+
+    var dt = DateTime.fromMillisecondsSinceEpoch(taskTimeStamp);
+
+    var stringDate = DateFormat('MM/dd/yyyy, hh:mm a').format(dt);
+    String url;
+
+    url = "http://dev.connect.cbs.lk/deleteComment.php";
+    var data = {
+      "comment_id": "$commentId",
+      "comment_delete_by": "$userName",
+      "comment_delete_by_id": "$name",
+      "action_taken_by": "$name",
+      "comment_delete_by_date": stringDate,
+      "comment_delete_by_timestamp": "$timeSt",
+    };
+
+    http.Response res = await http.post(
+      url,
+      body: data,
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      encoding: Encoding.getByName("utf-8"),
+    );
+
+    if (res.statusCode.toString() == "200") {
+      if (jsonDecode(res.body) == "true") {
+        setState(() {
+          getMainTaskCommentList(mainTaskId);
+        });
+        if (!mounted) return false;
+        snackBar(context, "Delete", Colors.redAccent);
+      }
+    } else {
+      if (!mounted) return false;
+      snackBar(context, "Error", Colors.redAccent);
+    }
+    return true;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    retrieverData();
+  }
+
+  void retrieverData() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      mainTaskId = (prefs.getString('main_task_id') ?? '');
+      mainTaskTitle = (prefs.getString('main_task_title') ?? '');
+      userName = (prefs.getString('user_name') ?? '');
+      userRole = (prefs.getString('user_role') ?? '');
+      firstName = (prefs.getString('first_name') ?? '').toUpperCase();
+      lastName = (prefs.getString('last_name') ?? '').toUpperCase();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +207,7 @@ class TaskDetailsDialog extends StatelessWidget {
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Text(
-                          '${task.taskTitle}',
+                          '${widget.task.taskTitle}',
                           style: TextStyle(
                             fontSize: 18,
                           ),
@@ -43,9 +219,9 @@ class TaskDetailsDialog extends StatelessWidget {
                                 onPressed: () {showDialog(
                                   context: context,
                                   builder: (BuildContext context) {
-                                    return   EditMainTask(lastName: '', firstName: '', userName: '', assign_to: '', company: task.company, documentNumber: task.documentNumber,
-                                      dueDate: task.dueDate, mainTaskId: task.taskId, sourceFrom: task.sourceFrom, taskCreateBy:task.taskCreateBy, taskCreateDate: task.taskCreateDate, taskCreatedTimestamp: task.taskCreatedTimestamp,
-                                      taskStatus: task.taskStatus, taskStatusName: task.taskStatusName, taskTitle: task.taskTitle, taskType: task.taskType, taskTypeName: task.taskTypeName,
+                                    return   EditMainTask(lastName: '', firstName: '', userName: '', assign_to: '', company: widget.task.company, documentNumber: widget.task.documentNumber,
+                                      dueDate: widget.task.dueDate, mainTaskId: widget.task.taskId, sourceFrom: widget.task.sourceFrom, taskCreateBy:widget.task.taskCreateBy, taskCreateDate: widget.task.taskCreateDate, taskCreatedTimestamp: widget.task.taskCreatedTimestamp,
+                                      taskStatus: widget.task.taskStatus, taskStatusName: widget.task.taskStatusName, taskTitle: widget.task.taskTitle, taskType: widget.task.taskType, taskTypeName: widget.task.taskTypeName,
 
                                     ); // Use the dialog widget here
                                   },
@@ -70,7 +246,7 @@ class TaskDetailsDialog extends StatelessWidget {
                       ],
                     ),
                     Text(
-                      '${task.taskId}',
+                      '${widget.task.taskId}',
                       style: TextStyle(
                         fontSize: 14,
                         color: Colors.black87,
@@ -190,7 +366,7 @@ class TaskDetailsDialog extends StatelessWidget {
                                     Padding(
                                       padding: const EdgeInsets.only(left: 4, bottom: 8, top: 8,right: 4 ),
                                       child: Text(
-                                        '${task.taskCreateDate}',
+                                        '${widget.task.taskCreateDate}',
                                         style: TextStyle(
                                             fontSize: 14,
                                             color: Colors.black,),
@@ -202,7 +378,7 @@ class TaskDetailsDialog extends StatelessWidget {
                                     Padding(
                                       padding: const EdgeInsets.only(left: 4, bottom: 8, top: 8,right: 4 ),
                                       child: Text(
-                                        '${task.dueDate}',
+                                        '${widget.task.dueDate}',
                                         style: TextStyle(
                                             fontSize: 14,
                                             color: Colors.black,),
@@ -215,7 +391,7 @@ class TaskDetailsDialog extends StatelessWidget {
                                 Padding(
                                   padding: const EdgeInsets.only(left: 18,bottom: 8),
                                   child: Text(
-                                    '${task.company}',
+                                    '${widget.task.company}',
                                     style: TextStyle(
                                         fontSize: 14,
                                         color: Colors.black,),
@@ -225,7 +401,7 @@ class TaskDetailsDialog extends StatelessWidget {
                                 Padding(
                                   padding: const EdgeInsets.only(left: 18,bottom: 8),
                                   child: Text(
-                                    '${task.assignTo}',
+                                    '${widget.task.assignTo}',
                                     style: TextStyle(
                                         fontSize: 14,
                                         color: Colors.black,),
@@ -235,7 +411,7 @@ class TaskDetailsDialog extends StatelessWidget {
                                 Padding(
                                   padding: const EdgeInsets.only(left: 18,bottom: 8),
                                   child: Text(
-                                    '${task.taskTypeName}',
+                                    '${widget.task.taskTypeName}',
                                     style: TextStyle(
                                         fontSize: 14,
                                         color: Colors.black,),
@@ -245,7 +421,7 @@ class TaskDetailsDialog extends StatelessWidget {
                                 Padding(
                                   padding: const EdgeInsets.only(left: 18,bottom: 8),
                                   child: Text(
-                                    '${task.taskStatusName}',
+                                    '${widget.task.taskStatusName}',
                                     style: TextStyle(
                                         fontSize: 14,
                                         color: Colors.black,),
@@ -255,7 +431,7 @@ class TaskDetailsDialog extends StatelessWidget {
                                 Padding(
                                   padding: const EdgeInsets.only(left: 18,bottom: 8),
                                   child: Text(
-                                    '${task.taskCreateBy}',
+                                    '${widget.task.taskCreateBy}',
                                     style: TextStyle(
                                         fontSize: 14,
                                         color: Colors.black,),
@@ -356,6 +532,59 @@ class TaskDetailsDialog extends StatelessWidget {
                       width: 330,
                       height: 225,
                       color: Colors.white,
+                      child: FutureBuilder<List<comment>>(
+                          future: getMainTaskCommentList(mainTaskId),
+                          builder: (context, snapshot) {
+                            if (snapshot.hasData) {
+                              List<comment> data = snapshot.data;
+                              return ListView.builder(
+                                  itemCount: data.length,
+                                  itemBuilder: (context, index) {
+                                    return Card(
+                                        child: ListTile(
+                                          title: SelectableText(
+                                            data[index].commnt,
+                                            style: TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 12),
+                                          ),
+                                          subtitle: Text(
+                                            "${data[index].commentCreateDate}      ${data[index].commentCreateBy}",
+                                            style: TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 12,
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                          trailing: IconButton(
+                                            icon: Icon(
+                                              Icons.delete,
+                                              size: 14,
+                                              color: Colors.red,
+                                            ),
+                                            // highlightColor: Colors.pink,
+                                            onPressed: () {
+                                              if (data[index].commentCreateById ==
+                                                  userName) {
+                                                deleteCommentInMainTask(
+                                                    data[index].commentId,
+                                                    "0",
+                                                    userName,
+                                                    "$firstName $lastName");
+                                              } else {
+                                                snackBar(
+                                                    context,
+                                                    "You can't delete this comment",
+                                                    Colors.red);
+                                              }
+                                            },
+                                          ),
+                                        ));
+                                  });
+                            } else if (snapshot.hasError) {
+                              return const Text("-Empty-");
+                            }
+                            return const Text("Loading...");
+                          }),
                     ),
 
                     Container(
