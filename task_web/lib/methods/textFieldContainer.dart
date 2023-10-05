34e359becbc06dd252b2f05e0b2e44a87a6ca9dd
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2023-10-05 23:44:11 +0530

    User Creation Complete

---
+++ task_web/lib/methods/textFieldContainer.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class TextFieldContainer extends StatelessWidget {
+  final String topic;
+  final String hintText;
+  final TextEditingController controller;
+  const TextFieldContainer(
+      {super.key,
+      required this.topic,
+      required this.controller,
+      required this.hintText});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15),
+      color: Colors.white,
+      width: 370,
+      height: 60,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(2.0),
+            child: Text(
+              topic,
+              style: TextStyle(fontSize: 15),
+            ),
+          ),
+          Container(
+            width: 350,
+            height: 35,
+            child: TextField(
+              controller: controller,
+              decoration: InputDecoration(
+                alignLabelWithHint: true,
+                enabledBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                ),
+                focusedBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black, width: 1.0),
+                ),
+                hintText: hintText,
+                contentPadding: EdgeInsets.all( 5.0), // Adjust vertical padding
+              ),
+            )
+
+
+          ),
+        ],
+      ),
+    );
+  }
+}
--- task_web/lib/pages/users/userCreation.dart
+++ task_web/lib/pages/users/userCreation.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
+import '../../drawers/adminSubDrawer.dart';
+import '../../drawers/userDrawer.dart';
+import '../../methods/appBar.dart';
+import '../../methods/colors.dart';
+import '../../methods/textFieldContainer.dart';
+
 class UserCreation extends StatefulWidget {
   const UserCreation({Key? key}) : super(key: key);
 
@@ -8,8 +14,145 @@ class UserCreation extends StatefulWidget {
 }
 
 class _UserCreationState extends State<UserCreation> {
+
+  TextEditingController firstNameCreateController = TextEditingController();
+  TextEditingController lastNameCreateController = TextEditingController();
+  TextEditingController emailCreateController = TextEditingController();
+  TextEditingController passwordCreateController = TextEditingController();
+  TextEditingController contactCreateController = TextEditingController();
+  TextEditingController employeeIDCreateController = TextEditingController();
+  TextEditingController designationCreateController = TextEditingController();
+  TextEditingController companyCreateController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(kToolbarHeight),
+        child: MyAppBar(),
+      ),
+
+      body: Row(
+        children: [
+          UserDrawer(),
+          AdminSubDrawer(),
+
+          Column(
+            children: [
+              Container(
+                margin: EdgeInsets.all(30),
+                width: 800,
+                height: 500,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey, // Shadow color
+                      blurRadius: 5, // Spread radius
+                      offset: Offset(0, 3), // Offset in x and y directions
+                    ),
+                  ],
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Row(
+                      children: [
+                        TextFieldContainer(topic: 'First Name:', controller: firstNameCreateController, hintText: '',),
+                        TextFieldContainer(topic: 'Last Name:', controller: lastNameCreateController, hintText: '',),
+                      ],
+                    ),
+
+                    Row(
+                      children: [
+                        TextFieldContainer(topic: 'Email:', controller: emailCreateController, hintText: '',),
+                        TextFieldContainer(topic: 'Password:', controller: passwordCreateController, hintText: '',),
+                      ],
+                    ),
+
+                    Row(
+                      children: [
+                        TextFieldContainer(topic: 'Contact Number:', controller: contactCreateController, hintText: '',),
+                        TextFieldContainer(topic: 'Employee ID:', controller: employeeIDCreateController, hintText: '',),
+                      ],
+                    ),
+
+                    Row(
+                      children: [
+                        TextFieldContainer(topic: 'Designation:', controller: designationCreateController, hintText: '',),
+                        TextFieldContainer(topic: 'Company:', controller: companyCreateController, hintText: '',),
+                      ],
+                    ),
+
+                    SizedBox(height: 50,),
+
+                    SizedBox(
+                      width: 750,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                       children: [
+                         Container(
+                           height: 40,
+                           width: 140,
+                           padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                           child: ElevatedButton(
+                             onPressed: () {
+                             },
+                             style: ElevatedButton.styleFrom(
+                               foregroundColor: AppColor.loginF,
+                               backgroundColor: Colors.lightBlue.shade50,
+                               shape: RoundedRectangleBorder(
+                                 borderRadius: BorderRadius.circular(5), // Rounded corners
+                               ),
+                             ),
+                             child: const
+                             Text('Create',
+                               style: TextStyle(
+                                 fontWeight: FontWeight.bold,
+                               ),
+                             ),
+                           ),
+
+
+                         ),
+
+                         Container(
+                           height: 40,
+                           width: 140,
+                           padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                           child: ElevatedButton(
+                             onPressed: () {
+                             },
+                             style: ElevatedButton.styleFrom(
+                               foregroundColor: AppColor.loginF,
+                               backgroundColor: Colors.lightBlue.shade50,
+                               shape: RoundedRectangleBorder(
+                                 borderRadius: BorderRadius.circular(5), // Rounded corners
+                               ),
+                             ),
+                             child: const
+                             Text('Cancel',
+                               style: TextStyle(
+                                 fontWeight: FontWeight.bold,color: Colors.redAccent
+                               ),
+                             ),
+                           ),
+
+
+                         ),
+
+
+                       ],
+                      ),
+                    )
+                  ],
+                ),
+              )
+
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
